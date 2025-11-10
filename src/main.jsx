ccc31b5f180048e3738ad24c8fcba93b74271b38
wttvitoria <168997485+wttvitoria@users.noreply.github.com>
2025-11-10 11:12:48 -0300

    add HelmetProvider fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; 
 import App from '@/App';
 import '@/index.css';
 import { AuthProvider } from '@/contexts/SupabaseAuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </>
+  <React.StrictMode>
+    <HelmetProvider> 
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
 );
