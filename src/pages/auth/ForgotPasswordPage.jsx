0b05f971964da4526fc10bef30ee608b68e5bcce
wttvitoria <168997485+wttvitoria@users.noreply.github.com>
2025-11-10 09:22:01 -0300

    Change Helmet import to react-helmet-async

--- src/pages/auth/ForgotPasswordPage.jsx
+++ src/pages/auth/ForgotPasswordPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Mail, ArrowLeft, Send, Loader2, ArrowUpDown } from 'lucide-react';
